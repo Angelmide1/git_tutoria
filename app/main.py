6cf4b8e894f78556e8463f3f698ae6a9639969e2
Angelmide1 <ayomideolayinka509@gmail.com>
2024-10-02 16:09:04 +0100

    streamlit to develop website

---
+++ app/main.py
@@ -0,0 +1,20 @@
+#streamlit web file
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+st.set_page_config(
+    layout= "wide",
+    initial_sidebar_state="expanded",
+    page_title="Predictprice"
+)
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title= None,
+        options= ["Home", "Prediction", "Dashboard"],
+        icons= ["house", "", ""],
+        menu_icon= "cast",
+        default_index= 0
+    )
